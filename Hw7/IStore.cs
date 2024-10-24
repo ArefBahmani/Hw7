0c42ddf87b4bcefef032b8af9dc083a7c946e72b
AREF BAHMANI <arefbahmani021@gmail.com>
2024-10-24 09:55:44 +0330

    Add project files.

---
+++ Hw7/IStore.cs
@@ -0,0 +1,14 @@
+﻿
+public interface IStore
+{
+    public List<Product> GetAllProducts();
+    public void AddProductToCart(Product product, int count);
+    public void ViewShoppingCart();
+    public void Checkout();
+    public void ShowFactor(int userId);
+    public void Increase(int amount);
+
+
+
+}
+
